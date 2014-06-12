4093dcf2c9f927c2cb7eeb6bf80804f53ac7aa94
Prateem Shrestha <prateems@gmail.com>
2014-06-12 09:50:37 -0400

    Switched editing to Form Model Binding, just because. Pretty cool.

--- app/views/admin/announcements/edit.blade.php
+++ app/views/admin/announcements/edit.blade.php
@@ -1,19 +1,19 @@
 <div class="row">
   <h1>Edit Announcement "{{{ $announcement->title }}}"</h1>
 
-  {{ Form::open(['route' => ['admin.announcements.update', $announcement->slug], 'method' => 'put']) }}
+  {{ Form::model($announcement, ['route' => ['admin.announcements.update', $announcement->slug], 'method' => 'put']) }}
 
   <div class="row">
     <div class="medium-6 columns">
       {{ Form::label('title', 'Title') }}
-      {{ Form::text('title', $announcement->title) }}
+      {{ Form::text('title') }}
     </div>
   </div>
 
   <div class="row">
     <div class="medium-10 columns">
       {{ Form::label('body', 'Content') }}
-      {{ Form::textarea('body', $announcement->body) }}
+      {{ Form::textarea('body') }}
     </div>
   </div>
 
