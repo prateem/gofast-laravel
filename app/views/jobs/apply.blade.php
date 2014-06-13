1b74be4b84dc6f2bb27247d8a5e9e39ec4aaa478
Jason Somai <jsomai458@gmail.com>
2014-06-13 00:41:07 -0400

    added breadcrumbs to apply, buttons on show to apply, links

--- app/views/jobs/apply.blade.php
+++ app/views/jobs/apply.blade.php
@@ -1,9 +1,14 @@
+<br>
 <div class="row">
-  <div class="large-12 columns">
-    <h1>Apply for {{{ $job->title }}}</h1>
-  </div>
+  <ul class="breadcrumbs small-12 columns">
+    <li>{{ link_to_route('home', 'Home') }}</li>
+    <li>{{ link_to_route('jobs.index', 'Jobs') }}</li>
+    <li>{{ link_to_route('jobs.show', $job->title, $job->id) }}</li>
+    <li class="current">Apply</li>
+  </ul>
 </div>
 <div class="row">
+  <h1>Apply for {{{ $job->title }}}</h1>
   {{ Form::open(['action' => 'JobController@apply']) }}
   <div class="row">
     <div class="small-6 columns">
@@ -35,6 +40,11 @@
     <div class="small-12 columns">
       {{ Form::label('upload', 'Upload') }}
       {{ Form::file('resume') }}
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-12 columns">
+      {{ Form::submit('Submit', ['class' => 'button']) }}
     </div>
   </div>
   {{ Form::close() }}
--- app/views/jobs/show.blade.php
+++ app/views/jobs/show.blade.php
@@ -30,7 +30,9 @@
 
     <h3>Requirements</h3>
     <p>{{{ $job->requirements }}}</p>
-
-    {{ link_to_route('jobs.index', 'View All', null, ['class' => 'button right']) }}
+  <ul class="button-group">
+    <li>{{ link_to_route('jobs.apply', 'Apply', $job->id, ['class' => 'button right']) }}</li>
+    <li>{{ link_to_route('jobs.index', 'View All', null, ['class' => 'button right']) }}</li>
+  </ul>
   </div>
 </div>
